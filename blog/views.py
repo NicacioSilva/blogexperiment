8b45268b4042c522137d45c29a345b6f1faf8313
NicacioSilva <nas@cesar.school>
2020-05-16 15:05:51 -0300

    easily add a home and about page to our blog.

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</>')
+    return render(request, 'blog/home.html')
+
+
+def about(request):
+    return render(request, 'blog/about.html')
