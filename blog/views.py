ac18f24413c715172b1d918e8c284dd35178041d
NicacioSilva <nas@cesar.school>
2020-05-16 18:07:19 -0300

    display some content using loops.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Nicacio',
+        'title': 'Frist Post',
+        'content': 'First post content',
+        'date_posted': 'May 16, 2020'
+    },
+    {
+        'author': 'Rebeca',
+        'title': 'Second Post',
+        'content': 'Second post content',
+        'date_posted': 'May 17, 2020'
+    },
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
